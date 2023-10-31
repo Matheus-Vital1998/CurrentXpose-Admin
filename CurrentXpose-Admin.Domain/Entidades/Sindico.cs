d4c100bd6758137ceec5caa3c54fbc70b16f033e
Matheus Oliveira <fh185@t-systems.com.br>
2023-10-31 00:54:43 -0300

    [FEAT] - Definindo as entidades

---
+++ CurrentXpose-Admin.Domain/Entidades/Sindico.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrentXpose_Admin.Domain.Entidades
+{
+    public class Sindico
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+        public string login { get; set; }
+        public string senha { get; set; }
+        public Condominio condominio { get; set; }
+        public Tipo_Relatorio nivel_relatorio { get; set; }
+
+        public Sindico() { }
+    }
+}
